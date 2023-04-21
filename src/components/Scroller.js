cd44d3c72ff5598fdaaa01f4759956fc33a634f4
anondegengpt <anondegengpt@gmail.com>
2023-04-21 14:54:07 -0700

    scroller arrows changed colors

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -9,18 +9,18 @@ export default function Scroller() {
     <div className="scroller-container">
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         <SwiperSlide>
-            Phase 0 <br/><br/>
-            - Initial MVPs<br/>  - Website + socials<br/> - Token Stealth launch<br/>
+          Phase 0 <br /><br />
+          - Initial MVPs<br />  - Website + socials<br /> - Token Stealth launch<br />
         </SwiperSlide>
         <SwiperSlide>
-            Phase 1 <br/><br/>
-            - New Token finder powered by AI<br/>  - Community activations<br/>  - Partnerships with existing crypto<br/>    communities<br/>  - Utility token integration<br/>
+          Phase 1 <br /><br />
+          - New Token finder powered by AI<br />  - Community activations<br />  - Partnerships with existing crypto<br />    communities<br />  - Utility token integration<br />
         </SwiperSlide> <SwiperSlide>
-            Phase 2 <br/><br/>
-            - Major brand partnerships<br/> - AI powered NFT bot<br/> - Full scale degen web application<br/> - Expand revenue model<br/>
+          Phase 2 <br /><br />
+          - Major brand partnerships<br /> - AI powered NFT bot<br /> - Full scale degen web application<br /> - Expand revenue model<br />
         </SwiperSlide> <SwiperSlide>
-            Phase 3 <br/><br/>
-            - Increase business revenue<br/>- More major partnerships<br/>
+          Phase 3 <br /><br />
+          - Increase business revenue<br />- More major partnerships<br />
         </SwiperSlide>
 
       </Swiper>
