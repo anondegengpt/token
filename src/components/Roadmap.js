fec2d1282237cd4665b3dbfe2fab5210d5e57704
anondegengpt <anondegengpt@gmail.com>
2023-04-21 14:27:41 -0700

    finished making the site mobile friendly including the footstep

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,35 +1,55 @@
-import './Roadmap.css'; 
-import React, { useRef, useEffect} from 'react';
+import './Roadmap.css';
+import React, { useRef, useEffect, useState } from 'react';
 import FootstepsAnimation from './Footsteps';
+import Scroller from './Scroller';
 
-export default function Roadmap () {
+
+export default function Roadmap() {
   const canvasRef = useRef(null);
+  const [showScroller, setShowScroller] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 900) {
+        setShowScroller(true);
+      } else {
+        setShowScroller(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
 
   const draw = () => {
     const canvas = canvasRef.current;
+    if (!canvas) return;
     const ctx = canvas.getContext('2d');
-  
+
     // Set canvas dimensions and clear previous drawings
     canvas.width = window.innerWidth - 250;
-    canvas.height = 500;
+    canvas.height = 300;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
     // Draw horizontal line
     ctx.beginPath();
     ctx.moveTo(0, 50); // Center line vertically on canvas
     ctx.lineTo(canvas.width, 50);
     ctx.strokeStyle = 'white';
     ctx.stroke();
-  
+
     // Draw four columns attached to the line
     const columnWidth = canvas.width / 4;
     const columnLabels = ['Phase 0', 'Phase 1', 'Phase 2', 'Phase 3'];
-    const columnText = [    `  - Initial MVPs\n  - Website + socials\n  - Token Stealth launch\n`,    '  - New Token finder powered by AI \n  - Community activations \n  - Partnerships with existing crypto\n    communities\n  - Utility token integration ',    ' - Major brand partnerships\n - AI powered NFT bot\n - Full scale degen web application \n - Expand revenue model',    ' - Increase business revenue\n - More major partnerships '  ];
-  
+    const columnText = [`  - Initial MVPs\n  - Website + socials\n  - Token Stealth launch\n`, '  - New Token finder powered by AI \n  - Community activations \n  - Partnerships with existing crypto\n    communities\n  - Utility token integration ', ' - Major brand partnerships\n - AI powered NFT bot\n - Full scale degen web application \n - Expand revenue model', ' - Increase business revenue\n - More major partnerships '];
+
     for (let i = 0; i < 4; i++) {
       // Calculate text height and set column height
       const lineHeight = 20;
-  
+
       // Draw column
       const x = i * columnWidth + columnWidth / 2;
       const y = 60;
@@ -38,19 +58,19 @@ export default function Roadmap () {
       ctx.arc(x, y, radius, 0, 2 * Math.PI);
       ctx.fillStyle = "transparent"
       ctx.fill();
-  
+
       // Draw column dot
       ctx.beginPath();
       ctx.arc(x + 10, y - 10, radius, 0, 2 * Math.PI);
       ctx.fillStyle = "white"
       ctx.fill();
-  
-  
+
+
       // Draw column label
       ctx.font = '16px Noto Sans JP';
       ctx.fillStyle = 'white';
       ctx.fillText(columnLabels[i], x - 15, y + 25);
-  
+
       // Draw column text aligned left
       ctx.font = '14px Noto Sans JP';
       ctx.textAlign = 'left';
@@ -58,14 +78,14 @@ export default function Roadmap () {
       const lines = columnText[i].split('\n');
       for (let j = 0; j < lines.length; j++) {
         ctx.fillText(lines[j], x - 95, y + 55 + lineHeight * j);
-      }      
+      }
     }
   };
-  
+
   const handleMouseMove = (event) => {
     draw();
   };
-  
+
   useEffect(() => {
     draw();
     window.addEventListener('mousemove', handleMouseMove);
@@ -73,29 +93,39 @@ export default function Roadmap () {
       window.removeEventListener('mousemove', handleMouseMove);
     };
   });
-  
-  return ( 
+
+  return (
     <div className='roadmap-container'>
-       <div className="roadmap-title">
-       <div class="divider">
-        <div class="line"></div>
-        <div class="circle"></div>
-        <div class="rectangle"></div>
-        <div class="circle"></div>
-        <div class="line"></div>
-      </div>
+
+      <div className="roadmap-title">
+        <div class="divider">
+          <div class="line"></div>
+          <div class="circle"></div>
+          <div class="rectangle"></div>
+          <div class="circle"></div>
+          <div class="line"></div>
+        </div>
         <p>ROADMAP</p>
-        </div>
-        <div className='dotted-border'>
-          <div className='footsteps'>
+      </div>
+
+      <div className='dotted-border'>
+        <div className='footsteps'>
+          {window.innerWidth < 767 ? (
+            <FootstepsAnimation numFootsteps={3} />
+          ) : (
             <FootstepsAnimation numFootsteps={8} />
-          </div>
+          )}
+        </div>
+      </div>
+
+      {showScroller ? (
+        <Scroller />
+      ) : (
+        <div className='line-with-columns'>
+          <canvas ref={canvasRef} />
         </div>
 
-
-    <div className='line-with-columns'>
-      <canvas ref={canvasRef} />
-    </div>
+      )}
     </div>
   )
 }
---
+++ src/components/Scroller.js
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Scroller.css";
+import { Navigation } from "swiper";
+
+export default function Scroller() {
+  return (
+    <div className="scroller-container">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+            Phase 0 <br/><br/>
+            - Initial MVPs<br/>  - Website + socials<br/> - Token Stealth launch<br/>
+        </SwiperSlide>
+        <SwiperSlide>
+            Phase 1 <br/>
+            - New Token finder powered by AI<br/>  - Community activations<br/>  - Partnerships with existing crypto<br/>    communities<br/>  - Utility token integration<br/>
+        </SwiperSlide> <SwiperSlide>
+            Phase 2 <br/>
+            - Major brand partnerships<br/> - AI powered NFT bot<br/> - Full scale degen web application<br/> - Expand revenue model<br/>
+        </SwiperSlide> <SwiperSlide>
+            Phase 3 <br/>
+            - Increase business revenue<br/>- More major partnerships<br/>
+        </SwiperSlide>
+
+      </Swiper>
+    </div>
+  );
+}
